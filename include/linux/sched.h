venoomdev/VenoomCI

sched: Import SONY's scheduler optimizations
- Imported scheduler optimizations from Sony Xperia 1 II (PDX203) 58.2.A.7.93 kernel copyleft.
- Introduced SONY_SCHED Kconfig option to compile SONY's scheduler codes conditionally at compile time.

Originally, PF_MIGRATE_ED_TASK at include/linux/sched.h had 0x02000000 as its process flag
at SONY's repository. But that clashes with PF_PERF_CRITICAL in our kernel. Hence move
PF_MIGRATE_ED_TASK to top and set 0x00004000 as its process flag to avoid conflicts and
thus remove the ambiguity. This change can be restored for those who don't have PF_PERF_CRITICAL
and doesn't have PF_MIGRATE_ED_TASK's process flag pre-occupied by some other flags.

Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

include/linux/sched.h
@@ -1720,6 +1720,9 @@ extern struct pid *cad_pid;
 #define PF_MEMALLOC		0x00000800	/* Allocating memory */
 #define PF_NPROC_EXCEEDED	0x00001000	/* set_user() noticed that RLIMIT_NPROC was exceeded */
 #define PF_USED_MATH		0x00002000	/* If unset the fpu must be initialized before use */
+#ifdef CONFIG_SONY_SCHED
+#define PF_MIGRATE_ED_TASK	0x00004000	/* Early detection task migration */
+#endif
 #define PF_NOFREEZE		0x00008000	/* This thread should not be frozen */
 #define PF_FROZEN		0x00010000	/* Frozen for system suspend */
 #define PF_KSWAPD		0x00020000	/* I am kswapd */
@@ -1733,7 +1736,11 @@ extern struct pid *cad_pid;
 #define PF_NO_SETAFFINITY	0x04000000	/* Userland is not allowed to meddle with cpus_allowed */
 #define PF_MCE_EARLY		0x08000000      /* Early kill for mce process policy */
 #define PF_WAKE_UP_IDLE         0x10000000	/* TTWU on an idle CPU */
+#ifdef CONFIG_SONY_SCHED
+#define PF_MIGRATE_CUM_ADJ_TASK	0x20000000	/* Cumulative task adjustment should be done */
+#else
 #define PF_MUTEX_TESTER		0x20000000	/* Thread belongs to the rt mutex tester */
+#endif
 #define PF_FREEZER_SKIP		0x40000000	/* Freezer should not count it as freezable */
 #define PF_SUSPEND_TASK		0x80000000      /* This thread called freeze_processes() and should not be frozen */
 

APOPHIS9283/Singularity_Kernel

Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

include/linux/sched.h
@@ -1870,10 +1870,17 @@ extern struct pid *cad_pid;
 #define PF_RANDOMIZE		0x00400000	/* Randomize virtual address space */
 #define PF_SWAPWRITE		0x00800000	/* Allowed to write to swap */
 #define PF_MEMSTALL		0x01000000	/* Stalled due to lack of memory */
+#ifdef CONFIG_SONY_SCHED
+#define PF_MIGRATE_ED_TASK      0x02000000      /* early detection task migration */
+#endif
 #define PF_NO_SETAFFINITY	0x04000000	/* Userland is not allowed to meddle with cpus_allowed */
 #define PF_MCE_EARLY		0x08000000      /* Early kill for mce process policy */
 #define PF_WAKE_UP_IDLE         0x10000000	/* TTWU on an idle CPU */
+#ifdef CONFIG_SONY_SCHED
+#define PF_MIGRATE_CUM_ADJ_TASK	0x20000000	/* Cumulative task adjustment should be done */
+#else
 #define PF_MUTEX_TESTER		0x20000000	/* Thread belongs to the rt mutex tester */
+#endif
 #define PF_FREEZER_SKIP		0x40000000	/* Freezer should not count it as freezable */
 #define PF_SUSPEND_TASK		0x80000000      /* This thread called freeze_processes() and should not be frozen */
 #if defined(OPLUS_FEATURE_PROCESS_RECLAIM) && defined(CONFIG_PROCESS_RECLAIM_ENHANCE)